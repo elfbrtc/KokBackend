3fd4766c61db5850fb6727aca24cf295318d3e7e
Elif Barutçu <elif.barutcu.5353@hotmail.com>
2023-09-22 21:46:23 +0300

    -connected mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 
 import express, { Express, Request, Response , Application } from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 //For env File 
 dotenv.config();
 
 const app: Application = express();
-const port = process.env.PORT || 8000;
+const db = mongoose.connect("mongodb+srv://elf:elf123@cluster0.lz40bun.mongodb.net/");
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Express & TypeScript Server');
