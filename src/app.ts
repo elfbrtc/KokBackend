375569a2e4628ce9cf1bed24c417f46cddc84c84
elfbrtc <elif.barutcu.5353@hotmail.com>
2023-10-04 16:30:17 +0300

    -created MongoDB model setup -adding express error handling

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,28 @@
 import "dotenv/config";
-import express, { Request, Response  } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import Notemodel from "./models/note";
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Welcome to Express & TypeScript Server');
+app.get('/', async (req, res, next) => {
+  try{
+    throw Error("errorrrssdd");
+    const notes = await Notemodel.find().exec();
+    res.status(200).json(notes)
+  }
+  catch(error){
+    next(error);
+  }  
+  });
+
+  app.use((req, res , next) => {
+    next(Error("Enpoint not found"))
+  })
+
+  app.use((error: unknown, req: Request, res: Response, next:NextFunction) => {
+    console.error(error);
+    let errMsg = "An unknown error occurred"
+    if (error instanceof Error)  errMsg = error.message;
+    res.status(500).json({error: errMsg});
   });
 
 export default app;
