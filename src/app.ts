25668fb5dd13f9024c9ed14d9e90d68bb358b487
elfbrtc <elif.barutcu.5353@hotmail.com>
2023-10-04 22:28:09 +0300

    -added controller, routes

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,18 @@
 import "dotenv/config";
-import express, { NextFunction, Request, Response } from 'express';
-import Notemodel from "./models/note";
+import express, { Request, Response } from 'express';
+import notesRoutes from "./routes/notes"
+
 const app = express();
 
-app.get('/', async (req, res, next) => {
-  try{
-    throw Error("errorrrssdd");
-    const notes = await Notemodel.find().exec();
-    res.status(200).json(notes)
-  }
-  catch(error){
-    next(error);
-  }  
-  });
+
+app.use("/api/notes", notesRoutes);
+
 
   app.use((req, res , next) => {
     next(Error("Enpoint not found"))
   })
 
-  app.use((error: unknown, req: Request, res: Response, next:NextFunction) => {
+  app.use((error: unknown, req: Request, res: Response) => {
     console.error(error);
     let errMsg = "An unknown error occurred"
     if (error instanceof Error)  errMsg = error.message;
