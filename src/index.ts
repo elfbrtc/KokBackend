ebde2aefcacdd943712ab56f035466a944417304
elfbrtc <elif.barutcu.5353@hotmail.com>
2023-11-25 21:30:31 +0300

    -file changes -added register

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,14 @@
-import env  from "./util/validateEnv"
 import mongoose from "mongoose";
-
 import express from 'express';
 import http from 'http';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors'
+import router from "./router"
 
 
 const app = express();
-
 app.use(cors({
   credentials: true,
 }))
@@ -21,15 +19,15 @@ app.use(bodyParser.json());
 
 const server = http.createServer(app)
 
+server.listen(8080, () =>{
+  console.log("server fire")
+})
+;
+const MONGO_URL = 'mongodb+srv://elf:elf123@cluster0.lz40bun.mongodb.net/?retryWrites=true&w=majority'
 
-const port = env.PORT;
+mongoose.Promise=Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', ( error: Error)=>console.log(error))
 
-mongoose.connect(env.MONGO_CONNECTION_STRING)
-    .then(() => {
-      console.log("mongoose connected");
-      server.listen(port, () => {
-        console.log("Server is Fire at http://localhost:" + port);
-      });
-    })
-    .catch(console.error)
 
+app.use('./', router());
